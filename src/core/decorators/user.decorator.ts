76d85ba2faf1b6a631d4cd90e18a47e88a494f49
zhengzhongsu <zhengzuodi@aliyun.com>
2020-03-07 09:15:17 +0800

    delete all

--- src/core/decorators/user.decorator.ts
+++
@@ -1,7 +0,0 @@
-import { createParamDecorator } from '@nestjs/common';
-
-export const User = createParamDecorator((data, req) => {
-  // tslint:disable-next-line:no-console
-  console.log(data);
-  return req.user;
-});
--- src/posts/post.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsString } from 'class-validator';
-
-export class CreatePostDto {
-  @IsString()
-  readonly title: string;
-
-}
